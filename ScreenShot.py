311a2a0a3f398f4e1269407cb875c4c626b1b9c3
Your Name <you@example.com>
2023-05-26 18:35:00 +0300

    add new tasks

--- ScreenShot.py
+++ ScreenShot.py
@@ -1,6 +1,12 @@
-from mss import mss 
 
-with mss() as sct:
-    sct.shot()
+import pyautogui
+import time
+
+counter = 1
+while True:
+    im = pyautogui.screenshot()
+    im.save(f"Name{counter}.jpg")
+
+    time.sleep(60) # delay 60 sec = 1 minute
+    counter += 1 
     
-
